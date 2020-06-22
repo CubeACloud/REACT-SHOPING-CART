f018a8b045016d4ab2c567dfb7694877693ec6e9
Mudassir Khan <38735197+Muddi00seven@users.noreply.github.com>
2020-06-22 13:50:09 +0500

    DONE ,, NEED SOME MORE CHANGES FOR CAC

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
-// import './styles/app.scss';
-// import Header from './Components/Header';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-// import Cart from './Components/Cart';
-// import Home from './Components/Home';
-// import Contact from './Components/Contact';
-import Cart from './Components/cart';
-import Header  from './Components/header';
-import Home from './Components/home'
-
 import './styles/app.scss';
+import Header from './Components/Header';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Cart from './Components/Cart';
+import Home from './Components/Home';
+import Contact from './Components/Contact';
 
 
 function App() {
 
   return (
-<Router>
-  <Header/>
-  <Router path="/" exact component={Home}/>
-  <Router path="/card" exact component={Cart}/>
-
-</Router>
+    <div>
+      <Router>
+        <Header />
+        <Route path='/' exact component={Home} />
+        <Route exact path='/cart' component={Cart} />
+        <Route path='/contact' exact component={Contact} />
+      </Router>
+    </div>
 
   );
 }
--- src/cart system/Components/Home.js
+++ src/cart system/Components/Home.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import Banner from './banner'
-import Item from './items/items'
+import Banner from './Banner'
+import Item from './Items/Item'
 
 const Home = () => {
     return (
